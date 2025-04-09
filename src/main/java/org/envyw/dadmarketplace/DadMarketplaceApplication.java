802d810d17d1517255351b64c32028b08f4981af
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-04-09 17:01:12 +0900

    spring boot 3.4.4 initialize

---
+++ src/main/java/org/envyw/dadmarketplace/DadMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package org.envyw.dadmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/envyw/dadmarketplace/DadMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.envyw.dadmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
