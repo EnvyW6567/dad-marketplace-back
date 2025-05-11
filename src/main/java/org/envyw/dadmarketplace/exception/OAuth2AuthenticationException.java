a0c1d4559497ca554ddb42f73981bd9e5c70ddf7
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-05-11 23:35:42 +0900

    chore: getter 추가 및 사용하지 않는 메서드 삭제

--- src/main/java/org/envyw/dadmarketplace/exception/OAuth2AuthenticationException.java
+++ src/main/java/org/envyw/dadmarketplace/exception/OAuth2AuthenticationException.java
@@ -1,11 +1,13 @@
 package org.envyw.dadmarketplace.exception;
 
+import lombok.Getter;
 import org.envyw.dadmarketplace.exception.errorCode.AuthErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
+@Getter
 public class OAuth2AuthenticationException extends AuthenticationException {
     private final String errorCode;
 
@@ -17,10 +19,5 @@ public class OAuth2AuthenticationException extends AuthenticationException {
     public OAuth2AuthenticationException(AuthErrorCode authErrorCode) {
         super(authErrorCode.formatMessage());
         this.errorCode = authErrorCode.getCode();
-    }
-
-    public OAuth2AuthenticationException(String msg, String errorCode) {
-        super(msg);
-        this.errorCode = errorCode;
     }
 }
