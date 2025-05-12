ef8add8194cc1e4f8dac831866f4d14301c1e5c4
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-05-12 15:08:32 +0900

    feat: 성공하는 CustomOAuth2LoginSuccessHandler 작성
    - ServerAuthenticationSuccessHandler 인터페이스 구현체
    
    - avatar 값이 없으면 default-image로 적용
    - 인증 성공 시 '/' 페이지로 리다이렉션 할 수 있도록 응답

--- src/main/java/org/envyw/dadmarketplace/security/dto/CustomOAuth2LoginSuccessHandler.java
+++ src/main/java/org/envyw/dadmarketplace/security/dto/CustomOAuth2LoginSuccessHandler.java
@@ -1,19 +1,49 @@
 package org.envyw.dadmarketplace.security.dto;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import reactor.core.publisher.Mono;
 
-public class CustomOAuth2LoginSuccessHandler {
+import java.net.URI;
+import java.util.Optional;
 
-    public DiscordUserDto extractDiscordUserInfo(OAuth2User oAuth2User) {
-        return new DiscordUserDto();
+@Slf4j
+public class CustomOAuth2LoginSuccessHandler implements ServerAuthenticationSuccessHandler {
+
+
+    public DiscordUserDto extractDiscordUserInfo(OAuth2User oauth2User) {
+        String id = oauth2User.getAttribute("id");
+        String username = oauth2User.getAttribute("username");
+        String avatar = oauth2User.getAttribute("avatar");
+        String email = oauth2User.getAttribute("email");
+
+        String avatarUrl = Optional.ofNullable(avatar)
+                .filter(a -> !a.isBlank())
+                .map(a -> String.format("https://cdn.discordapp.com/avatars/%s/%s.png", id, a))
+                .orElse("https://dafault-avatar-url.png");
+
+        return new DiscordUserDto(id, username, avatarUrl, email);
     }
 
-    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange,
-                                              OAuth2AuthenticationToken authentication) {
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        if (authentication instanceof OAuth2AuthenticationToken oauth2Token) {
+            OAuth2User oauth2User = oauth2Token.getPrincipal();
+            DiscordUserDto userInfo = extractDiscordUserInfo(oauth2User);
 
-        return null;
+            log.info("디스코드 사용자 인증 성공: id={}, username={}", userInfo.id(), userInfo.username());
+        }
+
+        ServerHttpResponse response = webFilterExchange.getExchange().getResponse();
+        response.setStatusCode(HttpStatus.FOUND);
+        response.getHeaders().setLocation(URI.create("/"));
+
+        return response.setComplete();
     }
 }
