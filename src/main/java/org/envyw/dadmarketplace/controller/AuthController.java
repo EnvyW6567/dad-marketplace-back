545f1e5702b935e46f87d4d0b7aa811f72a02b4a
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-06-14 17:41:06 +0900

    feat: 리프레시 토큰을 이용한 액세스 토큰 갱신 엔드포인트 구현
    - POST /api/auth/refresh
    - requestBody
      - {
        "refreshToken": "valid.refresh.token"
      }

--- src/main/java/org/envyw/dadmarketplace/controller/AuthController.java
+++ src/main/java/org/envyw/dadmarketplace/controller/AuthController.java
@@ -1,11 +1,12 @@
 package org.envyw.dadmarketplace.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.envyw.dadmarketplace.dto.request.RefreshReqDto;
+import org.envyw.dadmarketplace.dto.response.RefreshResDto;
+import org.envyw.dadmarketplace.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -17,10 +18,12 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @GetMapping("/login/discord")
     public Mono<Void> discordLogin(ServerWebExchange exchange) {
         ServerHttpResponse response = exchange.getResponse();
-        
+
         response.setStatusCode(HttpStatus.FOUND);
         response.getHeaders().setLocation(URI.create("/oauth2/authorization/discord"));
 
@@ -32,5 +35,10 @@ public class AuthController {
         return exchange.getPrincipal()
                 .map(principal -> Map.of("authenticated", true))
                 .defaultIfEmpty(Map.of("authenticated", false));
+    }
+
+    @PostMapping("/refresh")
+    public Mono<RefreshResDto> refresh(@RequestBody RefreshReqDto refreshReqDto) {
+        return authService.refresh(refreshReqDto);
     }
 }
