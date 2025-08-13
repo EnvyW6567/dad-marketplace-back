bc1e7a2468cc9be310f48049d476779400b81b4a
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-08-13 16:31:22 +0900

    refactor: dto 디렉토리 구조 변경
    - Attribute, Equipment, Rarity API DTO => searchKey

--- src/test/java/org/envyw/dadmarketplace/service/SearchKeywordServiceTest.java
+++ src/test/java/org/envyw/dadmarketplace/service/SearchKeywordServiceTest.java
@@ -1,9 +1,9 @@
 package org.envyw.dadmarketplace.service;
 
-import org.envyw.dadmarketplace.dto.AttributeDto;
-import org.envyw.dadmarketplace.dto.EquipmentDto;
-import org.envyw.dadmarketplace.dto.RarityDto;
 import org.envyw.dadmarketplace.dto.response.SearchKeywordResDto;
+import org.envyw.dadmarketplace.dto.searchKey.AttributeDto;
+import org.envyw.dadmarketplace.dto.searchKey.EquipmentDto;
+import org.envyw.dadmarketplace.dto.searchKey.RarityDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
