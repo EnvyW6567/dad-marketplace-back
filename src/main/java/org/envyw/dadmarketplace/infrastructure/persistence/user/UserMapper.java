9f187938baa44512fb6915c55836c46cc50c3754
EnvyW6567 <88361948+EnvyW6567@users.noreply.github.com>
2025-08-21 17:50:06 +0900

    fix: 로그인 성공 시 discordId 데이터가 비어있는 문제 해결
    - 디스코드 로그인 성공 시 discordId 값이 비어있는 문제 발견
    - findByDiscordId 내 UserEntity -> User 매핑 과정에서 discordId 값 주입 누락

--- src/main/java/org/envyw/dadmarketplace/infrastructure/persistence/user/UserMapper.java
+++ src/main/java/org/envyw/dadmarketplace/infrastructure/persistence/user/UserMapper.java
@@ -8,6 +8,7 @@ public class UserMapper {
     public User fromEntity(UserEntity userEntity) {
         return User.builder()
                 .userId(userEntity.getId())
+                .discordId(userEntity.getDiscordId())
                 .username(userEntity.getUsername())
                 .displayName(userEntity.getDisplayName())
                 .avatarUrl(userEntity.getAvatarUrl())
